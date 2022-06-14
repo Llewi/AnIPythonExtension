b09f581e69cf5669a4da9bd91e5f63b942332e9f
max may <max.mayrhauser@gmail.com>
2022-06-14 21:41:49 +0200

    add methods

--- src/example_magic/abracadabra.py
+++ src/example_magic/abracadabra.py
@@ -1,4 +1,4 @@
-from IPython.core.magic import (Magics, magics_class, line_magic, cell_magic)
+from IPython.core.magic import (Magics, magics_class, line_magic, cell_magic, display, HTML)
 
 @magics_class
 class Abracadabra(Magics):
@@ -10,3 +10,11 @@ class Abracadabra(Magics):
     @cell_magic
     def cadabra(self, line, cell):
         return line, cell
+
+    def vanilla_method():
+        return "literally no flavour"
+
+    @line_magic
+    def it_is_html():
+        return display(HTML('<h1>Hello, world!</h1>'))
+        
